22a31e60ec1751d54c3a6f6057f9d02820590cf1
DanilBaril <dapcoelho2708@gmail.com>
2026-03-06 17:25:25 +0000

    add input de nome, opçao lingua ingles e dark mode

--- app/src/main/java/dam_a51812/helloworld/MainActivity.kt
+++ app/src/main/java/dam_a51812/helloworld/MainActivity.kt
@@ -1,8 +1,13 @@
 package dam_a51812.helloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +21,52 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        var greetStr = "Olá"
+        var name = ""
+        var pt = true;
+
+        val buttonNome = findViewById<Button>(R.id.buttonNome)
+        val nomeBox = findViewById<EditText>(R.id.nomeBox)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val langBtn = findViewById<Button>(R.id.langBtn)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        buttonNome.setOnClickListener {
+            name = nomeBox.text.toString()
+            if(name.isEmpty()){ textView2.text = greetStr
+            }else{ textView2.text = "$greetStr $name!" }
+        }
+
+        langBtn.setOnClickListener {
+            if(pt){
+                langBtn.text = "EN"
+                greetStr = "Hello";textView2.text = "$greetStr $name!"
+                textView.text = "My first android app!"
+                nomeBox.setText("Name")
+                buttonNome.text = "Select"
+                pt = false
+            }else{
+                langBtn.text = "PT"
+                greetStr = "Olá";textView2.text = "$greetStr $name!"
+                textView.text = "A minha primeira app android!"
+                nomeBox.setText("Nome")
+                buttonNome.text = "Selecionar"
+                pt = true
+            }
+        }
+
+        val ldSwitch = findViewById<Switch>(R.id.LDswitch)
+        ldSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+
         println(getString(R.string.activity_oncreate_msg, this@MainActivity.localClassName))
     }
 }
